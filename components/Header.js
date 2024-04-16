c0fab3c242893ed2e6d756dbdb230b3de82da262
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-04-16 14:18:13 -0400

    portfolio nav

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,8 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
+    { id: 4, name: locale.NAV.PORTFOLIO, to: '/portfolio', show: true },
   ]
   return (
     <div className="flex-shrink-0">
